a59903a3420fb74f1bd27cab0189542499a246d7
mostafa jamal Eldeen <m_j_a_a@outlook.com>
2021-11-24 11:23:49 +0200

    test

---
+++ src/components/Layouts/Header/Lang.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="lang-menu">
+		<v-menu transition="scale-transition" :nudge-width="40">
+			<template v-slot:activator="{ on }">
+				<v-toolbar-title v-on="on">
+					<a href="javascript:void(0)" class="white--text font-weight-regular">{{selectedLocale.name}}</a>
+					<v-icon dark>arrow_drop_down</v-icon>
+				</v-toolbar-title>
+			</template>
+			<v-list class="py-0">
+				<v-list-item v-for="(language,key) in languages" :key="key" @click="changeLanguage(language)">
+					<span>{{language.name}}</span>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+	</div>
+</template>
+
+<script>
+	import { mapGetters } from "vuex";
+
+	export default {
+		computed: {
+			...mapGetters(["selectedLocale", "languages"])
+		},
+		methods: {
+			changeLanguage(language) {
+				this.$i18n.locale = language.locale;
+				this.$store.dispatch("changeLanguage", language);
+				if(language.locale == "ar" || language.locale == "fr")
+				{
+					this.$vuetify.rtl = true;
+					this.$store.dispatch("changeRtlLayout");
+				}
+				else
+				{
+					this.$vuetify.rtl = false;
+					this.$store.dispatch("changeRtlLayout");
+				}
+			}
+		}
+	}
+</script>
---
+++ src/lang/index.js
@@ -0,0 +1,27 @@
+import en from './en'
+import fr from './fr'
+import ar from './ar'
+import sp from './sp'
+import jp from './jp'
+import gr from './gr'
+
+export default {
+    en: {
+        message: en
+    },  
+    fr: {
+        message: fr
+    },  
+    ar: {
+        message: ar
+    },
+    sp: {
+        message: sp
+    },
+    jp: {
+        message: jp
+    },
+    gr:{
+        message: gr
+    }
+}
